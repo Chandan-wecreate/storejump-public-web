e8be0f181c261ce6e681132058730ec07748b255
Chandan-wecreate <chandan@wecreate.app>
2026-01-07 17:14:34 +0530

    base url hook update

--- src/hooks/useBaseUrl.ts
+++ src/hooks/useBaseUrl.ts
@@ -6,7 +6,7 @@ export default function useBaseUrl(url: string) {
     const [fullUrl, setFullUrl] = useState(url);
 
     useEffect(() => {
-        if (url && window.location.hostname === "localhost") {
+        if (!url?.includes("https") && typeof window !== "undefined" && window.location.hostname === "localhost") {
             setFullUrl(`http://localhost:1337${url}`);
         } else {
             setFullUrl(url);
